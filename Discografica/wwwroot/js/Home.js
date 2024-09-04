503d1aca4cad28305116c4d9bfddf5755392f640
ajaragon <julianaragongithub@outlook.es>
2024-09-04 10:38:05 +0200

    Se agregó la sección álbumes

--- Discografica/wwwroot/js/Home.js
+++ Discografica/wwwroot/js/Home.js
@@ -1,1 +1,8 @@
-﻿
+﻿// ****************** botones carrusel  ******************
+const carrusel = document.querySelector('#carrusel');
+botones = document.querySelectorAll('.seccion-contenedor i');
+botones.forEach(icon => {
+    icon.addEventListener('click', () => {
+        carrusel.scrollLeft += icon.id === "previo" ? -600 : 600; //432 = 27rem
+    });
+});
